d8f2472b08524b979afc5d129642379e95ab39e3
Mridul Agarwal <mridulagarwal160906@gmail.com>
2022-12-24 18:11:02 +0530

    Moved to extended patches for YouTube

--- utils.sh
+++ utils.sh
@@ -260,7 +260,7 @@ join_args() {
 build_youtube() {
 	declare -A youtube_args
 	youtube_args[app_name]="YouTube"
-	youtube_args[patcher_args]="-m ${RVNE_INTEGRATIONS_APK} $(join_args "${YOUTUBE_EXCLUDED_PATCHES}" -e) $(join_args "${YOUTUBE_INCLUDED_PATCHES}" -i)"
+	youtube_args[patcher_args]="-m ${RV_INTEGRATIONS_APK} $(join_args "${YOUTUBE_EXCLUDED_PATCHES}" -e) $(join_args "${YOUTUBE_INCLUDED_PATCHES}" -i)"
 	youtube_args[mode]="$YOUTUBE_MODE"
 	youtube_args[microg_patch]="microg-support"
 	youtube_args[pkg_name]="com.google.android.youtube"
@@ -268,10 +268,7 @@ build_youtube() {
 	youtube_args[apkmirror_dlurl]="google-inc/youtube"
 	youtube_args[regexp]="APK</span>[^@]*@\([^#]*\)"
 	youtube_args[module_prop_name]="ytrvx-magisk"
-	RV_PATCHES_JAR_BAK=$RV_PATCHES_JAR
-	RV_PATCHES_JAR=$RVNE_PATCHES_JAR
 	build_rv youtube_args
-	RV_PATCHES_JAR=$RV_PATCHES_JAR_BAK
 }
 
 #shellcheck disable=SC2034
@@ -319,7 +316,7 @@ build_reddit() {
 	declare -A reddit_args
 	reddit_args[app_name]="Reddit"
 	reddit_args[mode]="$REDDIT_MODE"
-	reddit_args[patcher_args]="-m ${RVNE_INTEGRATIONS_APK} $(join_args "${REDDIT_EXCLUDED_PATCHES}" -e) $(join_args "${REDDIT_INCLUDED_PATCHES}" -i)"
+	reddit_args[patcher_args]="-m ${RV_INTEGRATIONS_APK} $(join_args "${REDDIT_EXCLUDED_PATCHES}" -e) $(join_args "${REDDIT_INCLUDED_PATCHES}" -i)"
 	reddit_args[pkg_name]="com.reddit.frontpage"
 	reddit_args[apkmirror_dlurl]="redditinc/reddit"
 	reddit_args[regexp]='APK</span>[^@]*@\([^#]*\)'
